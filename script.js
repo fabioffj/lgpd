025336b298d90d3d5610480084268c3aa5f5808d
fabioffj <fabiojaenisch@homail.com>
2022-09-16 16:11:52 -0300

    Changed <h3> for <p> and added viewport

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function calcAndShowTotal() {
-  var total = 0;
-  $("#list :radio:checked").each(function () {
-    total += parseFloat($(this).attr("value")) || 0;
-  });
-  $("#total").val(total);
-}
-
-$("#list :radio").change(calcAndShowTotal).change();
+    var total = 0;
+    $("#list :radio:checked").each(function () {
+      total += parseFloat($(this).attr("value")) || 0;
+    });
+    $("#total").val(total);
+  }
+  
+  $("#list :radio").change(calcAndShowTotal).change();
